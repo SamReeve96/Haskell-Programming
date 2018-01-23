832a75d48802e5f448181dd7ad59673da0099f21
Samuel Reeve <sreeve96@googlemail.com>
2018-01-23 20:57:41 +0000

    Inc. worksheet 3 2 Ex's left

--- WorkSheet_3.hs
+++ WorkSheet_3.hs
@@ -1,11 +1,10 @@
---Ex 1
-
 --Heres a re definition of the && function using the same formate as || from the WorkSheet_3Extra.hs file
 
 -- We don't import '&&' from the prelude, so that we can 
 -- define our own version
 
-import Prelude hiding ((&&)) 
+import Prelude hiding ((&&))
+
 
 -- The following line declares the && operator (which we are about to
 -- re-define) to be right associative and to have precedence 3. This
@@ -34,13 +33,28 @@ False && False  = False
 --A note, && is an operator and so would be defined above like the 3 bottom lines
 -- Functions however dont need brackets and can be written like the thre lines after the 1st
 
+fact :: Int -> Int 
+fact n 
+    | n == 0    = 1
+    | n > 0     = n * fact (n - 1)
+    | otherwise = error "factorials not defined for negative ints"
+
+mult :: Int -> Int -> Int
+mult n m 
+    | n == 0        = 0
+    | n > 0         = m + mult (n - 1) m 
+
+divide :: Int -> Int -> Int
+divide n m
+    | n < m         = 0
+    | otherwise     = 1 + divide (n - m) m
 
 --Ex 2
 exOr :: Bool -> Bool -> Bool
 True `exOr` True = False
 exOr True False = True
 exOr False True = True
-exOr False False = True
+exOr False False = False
 
 --Ex 3 -> Always true???
 ifThenElse :: Bool -> Int -> Int -> Int 
@@ -63,4 +77,35 @@ daysInMonth 11 = 30
 daysInMonth 12 = 31
 
 --Ex 5
---sumNumbers :: Int -> Int
+sumNumbers :: Int -> Int
+sumNumbers n
+    |n > 0 = n + sumNumbers(n - 1)
+    |otherwise = 0
+
+--Ex 6
+sumSquares :: Int -> Int
+sumSquares n
+    |n > 0 = n ^ 2 + sumSquares(n - 1)
+    |otherwise = 0
+
+--Ex 7
+power :: Int -> Int -> Int
+power n p
+    |n > 0 && p > 1 = mult n (power n (p-1))
+    |otherwise = n
+
+--Ex 8
+sumFromTo :: Int -> Int -> Int
+sumFromTo x y
+    |y < x = 0
+    |otherwise = y + sumFromTo x (y-1)
+
+--Ex 9 How to hide gcd
+--gcd :: Int -> Int -> Int
+--gcd 
+
+--Ex 10
+--intSquareRoot :: Int -> Int
+--intSquareRoot x 
+--    |x < 0 = 0
+--   |otherwise = x
